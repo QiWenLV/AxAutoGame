5f4758fe004c6947c0be4d04e4428ac031a26eb7
kevin <1477945122@qq,com>
2022-09-13 22:21:58 +0800

    init

---
+++ src/admin/autdHandler.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger('link')
+
+
+class BaseAutoHandler():
+
+    def __init__(self, device_conn, s):
+        self.device_conn = device_conn
+        pass
+
+    def link_check(self):
+        pass
+
+    def link_device(self):
+        if self.device_conn is not None:
+            from automator.control.adb.targets import get_target_from_adb_serial
+            self._controller = get_target_from_adb_serial(adb_serial).create_controller()
+
+            self.link_check()
